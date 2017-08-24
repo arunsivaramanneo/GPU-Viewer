f8046a3596c79df79b583778b6f956f420299b79
Arun Sivaraman <arunsivaramanneo@gmail.com>
2017-08-25 00:01:21 +0530

    Memory Type Tab

--- vulkanviewer.py
+++ vulkanviewer.py
@@ -44,13 +44,17 @@ def Vulkan(tab2):
 	tabcontrol.add(FormatTab,text= "Formats")
 	tabcontrol.grid(column=0,row=1)
 
+	MemoryTypeTab = ttk.Frame(tabcontrol)
+	tabcontrol.add(MemoryTypeTab,text="Memory Type")
+	tabcontrol.grid(column=0,row=1)
+
 	radvar = tk.IntVar()
 
 	def Devices():
 		
 		TreeDevice = ttk.Treeview(DeviceTab,height=30,selectmode="browse")
 		TreeDevice['columns'] =('value')
-		TreeDevice.column('#0',width=220)
+		TreeDevice.column('#0',width=225)
 		TreeDevice.column('value',width=500)
 
 		TreeDevice.grid(column=0,row=0)
@@ -92,9 +96,9 @@ def Vulkan(tab2):
 		TreeFeatures = ttk.Treeview(FeatureTab,height=30)
 		TreeFeatures['columns'] =('value')
 		TreeFeatures.heading("#0", text='Device Features')
-		TreeFeatures.column('#0',width=520)
+		TreeFeatures.column('#0',width=525)
 		TreeFeatures.heading('value',text="Value")
-		TreeFeatures.column('value',width=200)
+		TreeFeatures.column('value',width=200,anchor='center')
 
 		TreeFeatures.grid(column=0,row=0)
 
@@ -142,9 +146,9 @@ def Vulkan(tab2):
 		TreeLimits = ttk.Treeview(LimitsTab,height = 30)
 		TreeLimits['columns'] = ('value')
 		TreeLimits.heading("#0", text='Device Limits')
-		TreeLimits.column('#0',width=520)
+		TreeLimits.column('#0',width=525)
 		TreeLimits.heading('value',text="Limits")
-		TreeLimits.column('value',width=200)
+		TreeLimits.column('value',width=200,anchor='sw')
 
 		TreeLimits.grid(column=0,row=0,sticky=tk.W)
 
@@ -183,11 +187,11 @@ def Vulkan(tab2):
 
 		te = ttk.Treeview(ExtensionsTab, height=30)
 		te.heading("#0", text='Name')
-		te.column('#0',width=522)
+		te.column('#0',width=525)
 		te['column'] = ('version')
 		te.grid(column=0,row=0)
 		te.heading('version',text="Version")
-		te.column('version',width=200)
+		te.column('version',width=200,anchor='center')
 
 		esb = ttk.Scrollbar(ExtensionsTab, orient="vertical", command=te.yview)
 		te.configure(yscrollcommand=esb.set)
@@ -269,6 +273,178 @@ def Vulkan(tab2):
 				
 		os.system("rm VKDFORMATS*.txt")
 
+	def MemoryTypes():
+
+		TreeMemory = ttk.Treeview(MemoryTypeTab,height=30)
+		TreeMemory['columns'] = ('value1','value2','value3','value4','value5','value6')
+		TreeMemory.heading('#0',text="Types")
+		TreeMemory.column('#0',width=50,anchor='center')
+		TreeMemory.heading('value1',text="Heap Index")
+		TreeMemory.column('value1',width=85,anchor="center")
+		TreeMemory.heading('value2',text="Device_Local")
+		TreeMemory.column('value2',width=110,anchor='center')
+		TreeMemory.heading('value3',text="Host_Visible")
+		TreeMemory.column('value3',width=110,anchor='center')
+		TreeMemory.heading('value4',text="Host_Coherent")
+		TreeMemory.column('value4',width=120,anchor='center')
+		TreeMemory.heading('value5',text="Host_Cached")
+		TreeMemory.column('value5',width=115,anchor='center')
+		TreeMemory.heading('value6',text="Lazily_Allocated")
+		TreeMemory.column('value6',width=135,anchor='center')
+
+		TreeMemory.grid(column=0,row=0)
+		
+		Mvsb = ttk.Scrollbar(MemoryTypeTab, orient="vertical", command=TreeMemory.yview)
+		TreeMemory.configure(yscrollcommand=Mvsb.set)
+		Mvsb.grid(column=0,row=0,sticky='nse')
+
+		GPU = radvar.get()
+		if GPU == 0 :
+			os.system("cat vulkaninfo.txt | awk '/GPU0/{flag=1;next}/VkPhysicalDeviceFeatures:/{flag=0}flag'|awk '/VkPhysicalDeviceMemoryProperties:/{flag=1; next}/VkPhysicalDeviceFeatures:/{flag=0} flag' | grep memoryTypes > VKDMemoryTypes.txt")
+			os.system("cat vulkaninfo.txt | awk '/GPU0/{flag=1;next}/VkPhysicalDeviceFeatures:/{flag=0}flag'|awk '/VkPhysicalDeviceMemoryProperties:/{flag=1; next}/VkPhysicalDeviceFeatures:/{flag=0} flag' | grep heapIndex | grep -o =.* | grep -o ' .*' > VKDMemoryHeapIndex.txt")
+			os.system("cat vulkaninfo.txt | awk '/GPU0/{flag=1;next}/VkPhysicalDeviceFeatures:/{flag=0}flag'|awk '/VkPhysicalDeviceMemoryProperties:/{flag=1; next}/VkPhysicalDeviceFeatures:/{flag=0} flag' | grep propertyFlags | grep -o =.* | grep -o ' .*' > VKDMemoryFlags.txt")
+		elif GPU == 1 :
+			os.system("cat vulkaninfo.txt | awk '/GPU1/{flag=1;next}/VkPhysicalDeviceFeatures:/{flag=0}flag'|awk '/VkPhysicalDeviceMemoryProperties:/{flag=1; next}/VkPhysicalDeviceFeatures:/{flag=0} flag' | grep memoryTypes > VKDMemoryTypes.txt")
+			os.system("cat vulkaninfo.txt | awk '/GPU1/{flag=1;next}/VkPhysicalDeviceFeatures:/{flag=0}flag'|awk '/VkPhysicalDeviceMemoryProperties:/{flag=1; next}/VkPhysicalDeviceFeatures:/{flag=0} flag' | grep heapIndex | grep -o =.* | grep -o ' .*' > VKDMemoryHeapIndex.txt")
+			os.system("cat vulkaninfo.txt | awk '/GPU1/{flag=1;next}/VkPhysicalDeviceFeatures:/{flag=0}flag'|awk '/VkPhysicalDeviceMemoryProperties:/{flag=1; next}/VkPhysicalDeviceFeatures:/{flag=0} flag' | grep propertyFlags | grep -o =.* | grep -o ' .*' > VKDMemoryFlags.txt")
+			
+		with open("VKDMemoryHeapIndex.txt","r") as file1:
+			heapIndex = []
+			for line in file1:
+				heapIndex.append(line)
+
+		Device_Local = []
+		Host_Visible = []
+		Host_Coherent = []
+		Host_Cached = []
+		Lazily_Allocated = []
+
+		with open("VKDMemoryFlags.txt","r") as file1:
+			for line in file1:
+				if line == " 0x0:\n":
+					Device_Local.append("false")
+					Host_Visible.append("false")
+					Host_Coherent.append("false")
+					Host_Cached.append("false")
+					Lazily_Allocated.append("false")
+
+				if line == " 0x1:\n":
+					Device_Local.append("true")
+					Host_Visible.append("false")
+					Host_Coherent.append("false")
+					Host_Cached.append("false")
+					Lazily_Allocated.append("false")
+
+				if line == " 0x2:\n":
+					Device_Local.append("false")
+					Host_Visible.append("true")
+					Host_Coherent.append("false")
+					Host_Cached.append("false")
+					Lazily_Allocated.append("false")
+				
+				if line == " 0x3:\n":
+					Device_Local.append("true")
+					Host_Visible.append("true")
+					Host_Coherent.append("false")
+					Host_Cached.append("false")
+					Lazily_Allocated.append("false")
+				
+				if line == " 0x4:\n":
+					Device_Local.append("false")
+					Host_Visible.append("false")
+					Host_Coherent.append("true")
+					Host_Cached.append("false")
+					Lazily_Allocated.append("false")
+				
+				if line == " 0x5:\n":
+					Device_Local.append("true")
+					Host_Visible.append("false")
+					Host_Coherent.append("true")
+					Host_Cached.append("false")
+					Lazily_Allocated.append("false")
+					
+				if line == " 0x6:\n":
+					Device_Local.append("false")
+					Host_Visible.append("true")
+					Host_Coherent.append("true")
+					Host_Cached.append("false")
+					Lazily_Allocated.append("false")
+					
+				if line == " 0x7:\n":
+					Device_Local.append("true")
+					Host_Visible.append("true")
+					Host_Coherent.append("true")
+					Host_Cached.append("false")
+					Lazily_Allocated.append("false")
+
+				if line == " 0x8:\n":
+					Device_Local.append("false")
+					Host_Visible.append("false")
+					Host_Coherent.append("false")
+					Host_Cached.append("true")
+					Lazily_Allocated.append("false")
+			
+				if line == " 0x9:\n":
+					Device_Local.append("true")
+					Host_Visible.append("false")
+					Host_Coherent.append("false")
+					Host_Cached.append("true")
+					Lazily_Allocated.append("false")
+			
+				if line == " 0xa:\n":
+					Device_Local.append("false")
+					Host_Visible.append("true")
+					Host_Coherent.append("false")
+					Host_Cached.append("true")
+					Lazily_Allocated.append("false")
+			
+				if line == " 0xb:\n":
+					Device_Local.append("true")
+					Host_Visible.append("true")
+					Host_Coherent.append("false")
+					Host_Cached.append("true")
+					Lazily_Allocated.append("false")
+			
+				if line == " 0xc:\n":
+					Device_Local.append("false")
+					Host_Visible.append("false")
+					Host_Coherent.append("true")
+					Host_Cached.append("true")
+					Lazily_Allocated.append("false")
+			
+				if line == " 0xd:\n":
+					Device_Local.append("true")
+					Host_Visible.append("false")
+					Host_Coherent.append("true")
+					Host_Cached.append("true")
+					Lazily_Allocated.append("false")
+								
+				if line == " 0xe:\n":
+					Device_Local.append("false")
+					Host_Visible.append("true")
+					Host_Coherent.append("true")
+					Host_Cached.append("true")
+					Lazily_Allocated.append("false")
+
+				if line == " 0xf:\n":
+					Device_Local.append("true")
+					Host_Visible.append("true")
+					Host_Coherent.append("true")
+					Host_Cached.append("true")
+					Lazily_Allocated.append("false")
+			
+			
+
+		with open("VKDMemoryTypes.txt","r") as file1:
+			count = len(file1.readlines())
+			tabcontrol.tab(5,text="Memory Types(%d)"%count)
+			file1.seek(0,0)
+			for i in range(count):
+				TreeMemory.insert('','end',text=i,values=(heapIndex[i],Device_Local[i],Host_Visible[i],Host_Coherent[i],Host_Cached[i],Lazily_Allocated[i]),tags=(Device_Local[i],Host_Visible[i],Host_Coherent[i],Host_Cached[i],Lazily_Allocated[i]))
+				
+
+		os.system("rm VKDMemory*.txt")
+
 	def radcall():
 		radsel= radvar.get()
 		if radsel == 0:
@@ -277,12 +453,14 @@ def Vulkan(tab2):
 			Limits()
 			Extensions()
 			Format()
+			MemoryTypes()
 		if radsel == 1:
 			Devices()
 			Features()
 			Limits()
 			Extensions()
 			Format()
+			MemoryTypes()
 
 
 	frame1 = ttk.LabelFrame(tab2,text="")
