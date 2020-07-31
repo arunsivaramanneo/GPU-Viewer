b3c211bb81103e8a9e9d0efceee7eea6acea754f
Arun Sivaraman <arunsivaramanneo@gmail.com>
2020-07-31 18:31:12 +0100

    1.26 Kick off

--- Files/GPUViewer.py
+++ Files/GPUViewer.py
@@ -1,6 +1,8 @@
-﻿#!/usr/bin/python3b
+﻿#!/usr/bin/python3
 import os
 import Const
+import os.path
+from os import path
 from Common import MyGtk, setScreenSize
 from OpenGLViewer import OpenGL
 from VulkanViewer import Vulkan
@@ -13,59 +15,61 @@ import time
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gio
 
+if path.exists("/tmp/gpu-viewer") == True:
+    pass
+else:
+    def main():
+        T1 = time.time()
 
-def main():
-    #T1 = time.time()
+        os.system("mkdir /tmp/gpu-viewer")
+        gtk = MyGtk("GPU-VIEWER")
+        setScreenSize(gtk, Const.WIDTH_RATIO, Const.HEIGHT_RATIO1)
 
-    os.system("mkdir /tmp/gpu-viewer")
-    gtk = MyGtk("GPU-VIEWER")
-    setScreenSize(gtk, Const.WIDTH_RATIO, Const.HEIGHT_RATIO1)
+        if isVulkanSupported():
+            vulkanTab = gtk.createTab(Const.VULKAN_PNG, Const.ICON_WIDTH, Const.ICON_HEIGHT, True)
+            t2 = threading.Thread(target=Vulkan, args=(vulkanTab,))
+            t2.start()
+            t2.join()
 
-    if isVulkanSupported():
-        vulkanTab = gtk.createTab(Const.VULKAN_PNG, Const.ICON_WIDTH, Const.ICON_HEIGHT, True)
-        t2 = threading.Thread(target=Vulkan, args=(vulkanTab,))
-        t2.start()
-        t2.join()
+        openGlTab = gtk.createTab(Const.OPEN_GL_PNG, Const.ICON_WIDTH, Const.ICON_HEIGHT, True)
+        t1 = threading.Thread(target=OpenGL, args=(openGlTab,))
+        t1.start()
+        t1.join()
 
-    openGlTab = gtk.createTab(Const.OPEN_GL_PNG, Const.ICON_WIDTH, Const.ICON_HEIGHT, True)
-    t1 = threading.Thread(target=OpenGL, args=(openGlTab,))
-    t1.start()
-    t1.join()
+        if isOpenclSupported():
+            openclTab = gtk.createTab(Const.OPEN_CL_PNG, 130, Const. ICON_HEIGHT, False)
+            t4 = threading.Thread(target=openCL, args=(openclTab,))
+            t4.start()
+            t4.join()
 
-    if isOpenclSupported():
-        openclTab = gtk.createTab(Const.OPEN_CL_PNG, 130, Const. ICON_HEIGHT, False)
-        t4 = threading.Thread(target=openCL, args=(openclTab,))
-        t4.start()
-        t4.join()
+        aboutTab = gtk.createTab(Const.ABOUT_US_PNG, Const.ICON_WIDTH, Const.ICON_HEIGHT, False)
+        t3 = threading.Thread(target=about, args=(aboutTab,))
+        t3.start()
+        t3.join()
 
-    aboutTab = gtk.createTab(Const.ABOUT_US_PNG, Const.ICON_WIDTH, Const.ICON_HEIGHT, False)
-    t3 = threading.Thread(target=about, args=(aboutTab,))
-    t3.start()
-    t3.join()
-
-    #print(time.time()-T1)
-    gtk.connect("delete-event", quit)
-    gtk.show_all()
-    gtk.mainLoop()
-
-
-def isOpenclSupported():
-    os.system("clinfo | awk '/Number of platforms/{flag=1;print}/NULL.*/{flag=0}flag' > /tmp/gpu-viewer/clinfo.txt")
-    with open("/tmp/gpu-viewer/clinfo.txt", "r") as file:
-        count = len(file.readlines())
-    return count > 2
+        print(time.time()-T1)
+        gtk.connect("delete-event", quit)
+        gtk.show_all()
+        gtk.mainLoop()
 
 
-def isVulkanSupported():
-    os.system("vulkaninfo > /tmp/gpu-viewer/vulkaninfo.txt")
-    with open("/tmp/gpu-viewer/vulkaninfo.txt", "r") as file1:
-        count = len(file1.readlines())
-    return count > 20
+    def isOpenclSupported():
+        os.system("clinfo | awk '/Number of platforms/{flag=1;print}/NULL.*/{flag=0}flag' > /tmp/gpu-viewer/clinfo.txt")
+        with open("/tmp/gpu-viewer/clinfo.txt", "r") as file:
+            count = len(file.readlines())
+        return count > 2
 
 
-def quit(instance, value):
-    os.system("rm /tmp/gpu-viewer -r")
-    instance.quit()
+    def isVulkanSupported():
+        os.system("vulkaninfo > /tmp/gpu-viewer/vulkaninfo.txt")
+        with open("/tmp/gpu-viewer/vulkaninfo.txt", "r") as file1:
+            count = len(file1.readlines())
+        return count > 20
 
 
-main()  # Program starts here
+    def quit(instance, value):
+        os.system("rm /tmp/gpu-viewer -r")
+        instance.quit()
+
+
+    main()  # Program starts here
--- Files/VulkanViewer.py
+++ Files/VulkanViewer.py
@@ -6,6 +6,7 @@ import Const
 
 import threading
 
+
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk, GdkPixbuf
 # noinspection PyPep8
