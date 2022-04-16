51041d087845f6e95c36267ea925d9c9feb38dbf
Arun Sivaraman <arunsivaramanneo@gmail.com>
2022-04-16 09:03:55 +0100

    icon width increase to 150

--- Files/Const.py
+++ Files/Const.py
@@ -88,7 +88,7 @@ FRAMEBUFFERLIST = ["vid", "vdep", "vt", "xsp", "bfsz", "lvl", "rt", "db", "st", 
                    "srgb", "aux", "depth", "stcl",
                    "acr", "acg", "acb", "aca", "msnum", "msbufs", "caveats"]
 
-ICON_WIDTH = 130
+ICON_WIDTH = 150
 
 ICON_HEIGHT = 50
 
