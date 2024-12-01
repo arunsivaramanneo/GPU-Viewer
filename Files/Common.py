54c49e5c1b0aed1030b222f4ff8ea98b27454025
Arun Sivaraman <arunsivaramanneo@gmail.com>
2024-12-01 18:23:01 +0000

    Update AMD Ryzen Logo

--- Files/Common.py
+++ Files/Common.py
@@ -303,7 +303,7 @@ def getLogo(line):
     elif "Radeon" in line and "Ryzen" not in line:
         logo_pixbuf = fetchImageFromUrl(const.Radeon_logo,32,28, False)
     elif "Ryzen" in line:
-        logo_pixbuf = fetchImageFromUrl(const.Ryzen_logo,32,28, True)
+        logo_pixbuf = fetchImageFromUrl(const.AMDRYZEN_LOGO_PNG,32,28, True)
     elif ("Mesa" in line or "radv" in line or "llvmpipe" in line or "dozen" in line or "venus" in line or "nvk" in line) and ("LLVM" not in line):
         logo_pixbuf = fetchImageFromUrl(const.Mesa_logo,32,28, True)
     elif "LLVM" in line :
