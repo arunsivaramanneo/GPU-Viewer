14bc7fe1bf24bbc609eef0d9d905712ce90086dc
Arun Sivaraman <arunsivaramanneo@gmail.com>
2025-06-21 16:10:22 +0100

    Fix invalid escape sequence error

--- Files/VulkanVideoViewer.py
+++ Files/VulkanVideoViewer.py
@@ -21,7 +21,7 @@ def VulkanVideo(videoTab):
     def vProfiles(gpu):
         
         vulkanVideoProfiles = fetchContentsFromCommand("vulkaninfo | awk '/GPU%d/{flag=1;next}/GPU%d/{flag=0}flag' | awk '/Video Profiles/{flag=1;}flag' | awk /./ | awk '!/===/' "%(gpu,gpu+1))
-        vulkanVideoProfilesUniq = fetchContentsFromCommand("vulkaninfo | awk '/GPU%d/{flag=1;next}/GPU%d/{flag=0}flag' | awk '/Video Profiles/{flag=1;next}flag' | awk /./ | awk '!/===/' | awk '{gsub(/\(.*/, 'True');print}' | uniq "%(gpu,gpu+1))
+        vulkanVideoProfilesUniq = fetchContentsFromCommand(r"vulkaninfo | awk '/GPU%d/{flag=1;next}/GPU%d/{flag=0}flag' | awk '/Video Profiles/{flag=1;next}flag' | awk /./ | awk '!/===/' | awk '{gsub(/\(.*/, 'True');print}' | uniq "%(gpu,gpu+1))
         
         vulkanVideoProfilesCount = []
         for i in range(len(vulkanVideoProfilesUniq)):
